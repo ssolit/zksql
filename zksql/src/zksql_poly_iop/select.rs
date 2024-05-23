b2ffa06ae97ce674911f6c9ed4c39890c82f401f
ssolit44 <ssolit44@gmail.com>
2024-05-23 18:29:38 -0400

    select_check refactored as its own function

--- zksql/src/zksql_poly_iop/select.rs
+++ zksql/src/zksql_poly_iop/select.rs
@@ -4,31 +4,69 @@
 // create table - vector of columns
 // run the product check on the table
 
-
-use  subroutines::{
-    pcs::PolynomialCommitmentScheme,
+use arithmetic::VirtualPolynomial;
+use ark_ff::{One, PrimeField};
+use ark_poly::{MultilinearExtension, DenseMultilinearExtension};
+use ark_std::{end_timer, start_timer};
+use subroutines::{
     poly_iop::{
         errors::PolyIOPErrors,
         zero_check::ZeroCheck,
         PolyIOP,
     },
+    IOPProof,
 };
-use arithmetic::VPAuxInfo;
-use ark_ec::pairing::Pairing;
-use ark_ff::{One, PrimeField, Zero};
-use ark_poly::{MultilinearExtension, DenseMultilinearExtension};
-use ark_std::{end_timer, start_timer};
-use std::sync::Arc;
-use transcript::IOPTranscript;
+use std::{sync::Arc, vec};
+
+
+
+fn select_check<F: PrimeField>(
+    table: &Vec<DenseMultilinearExtension<F>>,
+    sel: &DenseMultilinearExtension<F>,
+    result: &Vec<DenseMultilinearExtension<F>>,
+) -> Result<Vec<IOPProof<F>>, PolyIOPErrors> {
+
+    if table.len() != result.len() {
+        return Err(PolyIOPErrors::InvalidParameters(
+            "Table and result vectors must be the same length".to_string(),
+        ));
+    }
+
+    let mut proofs = vec![];
+    for i in 0..table.len() {
+        // build a virtual polynomial for the ZeroTest. v = (c1 * sel) - result_c1
+        let mut poly = VirtualPolynomial::new_from_mle(&Arc::new(table[i].clone()), F::one());
+        poly.mul_by_mle(Arc::new(sel.clone()), F::one()).unwrap();
+        poly.add_mle_list(vec![Arc::new(result[i].clone())], F::one().neg()).unwrap();
+
+        // invoke zero check
+        let mut transcript = <PolyIOP<F> as ZeroCheck<F>>::init_transcript();
+        transcript.append_message(b"testing", b"initializing transcript for testing")?;
+        let proof = <PolyIOP<F> as ZeroCheck<F>>::prove(&poly, &mut transcript)?;
+        proofs.push(proof);
+
+        // test verificiation
+        let poly_info = poly.aux_info.clone();
+        println!("poly info 1: {:?}", poly_info);
+
+        let mut ver_transcript = <PolyIOP<F> as ZeroCheck<F>>::init_transcript();
+        ver_transcript.append_message(b"testing", b"initializing transcript for testing")?;
+        let zero_subclaim =
+            <PolyIOP<F> as ZeroCheck<F>>::verify(&proofs[i].clone(), &poly_info, &mut ver_transcript)?;
+    }
+    
+    return Ok(proofs);
+}
+
 
 fn test_selection_proof() -> Result<(), PolyIOPErrors> { 
-    use ark_bls12_381::{Bls12_381, Fr};
+    // import specific types and other 
+    use ark_bls12_381::{Fr};
     use subroutines::MultilinearKzgPCS;
-    use arithmetic::VirtualPolynomial;
     use ark_std::test_rng;
-    use std::ops::Neg;
-
-
+    use ark_std::Zero;
+   
+    // create concrete values for testing 
     let nv = 2;
     let mut rng = test_rng();
 
@@ -65,31 +103,25 @@ fn test_selection_proof() -> Result<(), PolyIOPErrors> {
 
     let c1: DenseMultilinearExtension<Fr> = DenseMultilinearExtension::from_evaluations_slice(nv, &c1_elem);
     let c2: DenseMultilinearExtension<Fr> = DenseMultilinearExtension::from_evaluations_slice(nv, &c2_elem);
+    let table = vec![c1, c2];
     let sel: DenseMultilinearExtension<Fr> = DenseMultilinearExtension::from_evaluations_slice(nv, &sel_elem);
     let result_c1: DenseMultilinearExtension<Fr> = DenseMultilinearExtension::from_evaluations_slice(nv, &result_c1_elem);
     let result_c2: DenseMultilinearExtension<Fr> = DenseMultilinearExtension::from_evaluations_slice(nv, &result_c2_elem);
+    let result = vec![result_c1, result_c2];
     
-    // build a virtual polynomial for the ZeroTest. v = (c1 * sel) - result_c1
-    let mut c1_check_poly = VirtualPolynomial::new_from_mle(&Arc::new(c1), Fr::one());
-    c1_check_poly.mul_by_mle(Arc::new(sel), Fr::one()).unwrap();
-    c1_check_poly.add_mle_list(vec![Arc::new(result_c1)], Fr::one().neg()).unwrap();    
-
-    // todo: invoke zero check
-    let mut transcript = <PolyIOP<Fr> as ZeroCheck<Fr>>::init_transcript();
-    transcript.append_message(b"testing", b"initializing transcript for testing")?;
-    let proof = <PolyIOP<Fr> as ZeroCheck<Fr>>::prove(&c1_check_poly, &mut transcript)?;
-
-    let poly = c1_check_poly.clone();
-    let poly_info = poly.aux_info.clone();
-    let mut transcript = <PolyIOP<Fr> as ZeroCheck<Fr>>::init_transcript();
-    transcript.append_message(b"testing", b"initializing transcript for testing")?;
-    let zero_subclaim =
-        <PolyIOP<Fr> as ZeroCheck<Fr>>::verify(&proof, &poly_info, &mut transcript)?;
-    assert!(
-        poly.evaluate(&zero_subclaim.point)? == zero_subclaim.expected_evaluation,
-        "wrong subclaim"
-    );
-
+    // call select_check
+    let proofs = select_check::<Fr>(&table, &sel, &result)?;
+    
+    // verify proof passes
+    let mut poly_info = VirtualPolynomial::new_from_mle(&Arc::new(table[0].clone()), Fr::one()).aux_info.clone();
+    poly_info.max_degree += 1; // add one because shape gets larger from combining multiple virtual poly
+    for proof in proofs {
+        println!("here, len(proof.proofs): {}", proof.proofs.len());
+        let mut transcript = <PolyIOP<Fr> as ZeroCheck<Fr>>::init_transcript();
+        transcript.append_message(b"testing", b"initializing transcript for testing")?;
+        let zero_subclaim =
+            <PolyIOP<Fr> as ZeroCheck<Fr>>::verify(&proof, &poly_info, &mut transcript)?;
+    }
 
     // exit successfully 
     Ok(())
