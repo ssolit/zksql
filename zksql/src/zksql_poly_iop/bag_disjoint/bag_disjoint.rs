53655afdb1ef4ecb5f017cffd49d37b6f72b143f
ssolit44 <ssolit44@gmail.com>
2024-07-24 21:32:24 -0400

    refactored bag_disjoint to separate out prove and prove_with_advice

--- zksql/src/zksql_poly_iop/bag_disjoint/bag_disjoint.rs
+++ zksql/src/zksql_poly_iop/bag_disjoint/bag_disjoint.rs
@@ -1,5 +1,6 @@
 use ark_ec::pairing::Pairing;
 use std::marker::PhantomData;
+use std::cmp::max;
 
 use subroutines::pcs::PolynomialCommitmentScheme;
 use crate::{
@@ -7,6 +8,7 @@ use crate::{
     zksql_poly_iop::{
         bag_multitool::bag_inclusion::BagInclusionIOP,
         bag_sort::bag_sort::BagStrictSortIOP,
+        bag_disjoint::utils::calc_bag_disjoint_advice,
     },
 };
 
@@ -16,6 +18,32 @@ pub struct BagDisjointIOP<E: Pairing, PCS: PolynomialCommitmentScheme<E>>(Phanto
 impl <E: Pairing, PCS: PolynomialCommitmentScheme<E>> BagDisjointIOP<E, PCS> 
 where PCS: PolynomialCommitmentScheme<E> {
     pub fn prove(
+        prover_tracker: &mut ProverTrackerRef<E, PCS>, 
+        bag_a: &Bag<E, PCS>, 
+        bag_b: &Bag<E, PCS>, 
+        range_bag: &Bag<E, PCS>
+    ) -> Result<(), PolyIOPErrors> {
+        let (sum_mle, sum_sel_mle, sum_a_mult_mle, sum_b_mult_mle) = calc_bag_disjoint_advice(bag_a, bag_b)?;
+        let sum_poly = prover_tracker.track_and_commit_poly(sum_mle)?;
+        let sum_sel_poly = prover_tracker.track_and_commit_poly(sum_sel_mle)?;
+        let bag_c = Bag::new(sum_poly.clone(), sum_sel_poly.clone());
+        let sum_a_mult_poly = prover_tracker.track_and_commit_poly(sum_a_mult_mle)?;
+        let sum_b_mult_poly = prover_tracker.track_and_commit_poly(sum_b_mult_mle)?;
+
+        Self::prove_with_advice(
+            prover_tracker,
+            bag_a,
+            bag_b,
+            &bag_c,
+            &sum_a_mult_poly,
+            &sum_b_mult_poly,
+            &range_bag,
+        )?;
+
+        Ok(())
+    }
+
+    pub fn prove_with_advice(
         prover_tracker: &mut ProverTrackerRef<E, PCS>,
         bag_a: &Bag<E, PCS>,
         bag_b: &Bag<E, PCS>,
@@ -57,6 +85,37 @@ where PCS: PolynomialCommitmentScheme<E> {
     }
 
     pub fn verify(
+        verifier_tracker: &mut VerifierTrackerRef<E, PCS>,
+        bag_a: &BagComm<E, PCS>,
+        bag_b: &BagComm<E, PCS>,
+        range_bag: &BagComm<E, PCS>,
+    ) -> Result<(), PolyIOPErrors> {
+        let bag_sum_nv = max(bag_a.num_vars(), bag_b.num_vars()) + 1;
+        // get ids to transfer
+        let sum_poly_id = verifier_tracker.get_next_id();
+        let bag_c_poly = verifier_tracker.transfer_prover_comm(sum_poly_id);
+        let sum_sel_poly_id = verifier_tracker.get_next_id();
+        let bag_c_sel = verifier_tracker.transfer_prover_comm(sum_sel_poly_id);
+        let bag_c = BagComm::new(bag_c_poly, bag_c_sel, bag_sum_nv);
+        let ma_id = verifier_tracker.get_next_id();
+        let sum_a_mult_poly = verifier_tracker.transfer_prover_comm(ma_id);
+        let mb_id = verifier_tracker.get_next_id();
+        let sum_b_mult_poly = verifier_tracker.transfer_prover_comm(mb_id);
+
+        Self::verify_with_advice(
+            verifier_tracker,
+            bag_a,
+            bag_b,
+            &bag_c,
+            &sum_a_mult_poly,
+            &sum_b_mult_poly,
+            &range_bag,
+        )?;
+
+        Ok(())
+    }
+
+    pub fn verify_with_advice(
         verifier_tracker: &mut VerifierTrackerRef<E, PCS>,
         bag_a: &BagComm<E, PCS>,
         bag_b: &BagComm<E, PCS>,
