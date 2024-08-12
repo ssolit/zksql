ddc750d615de6ad5d10f190dfdf4f03a11eaea15
ssolit44 <ssolit44@gmail.com>
2024-08-12 13:15:20 -0400

    drafted final_join_one_to_many

---
+++ zksql/src/zksql_poly_iop/final_join_one_to_many/mod.rs
@@ -0,0 +1,3 @@
+pub mod final_join_one_to_many;
+pub mod utils;
+mod test;
---
+++ zksql/src/zksql_poly_iop/final_join_one_to_many/utils.rs
@@ -0,0 +1,117 @@
+use ark_ec::pairing::Pairing;
+use ark_poly::DenseMultilinearExtension;
+use std::marker::PhantomData;
+use ark_std::One;
+use ark_std::Zero;
+use std::ops::Neg;
+use std::collections::HashMap;
+
+use subroutines::pcs::PolynomialCommitmentScheme;
+use crate::{
+    tracker::prelude::*,
+    zksql_poly_iop::{
+        bag_disjoint::bag_disjoint::BagDisjointIOP, bag_inclusion::bag_inclusion::BagInclusionIOP, index_transform, selector_valid::selector_valid::SelectorValidIOP
+    },
+};
+
+// // Calculates the transformed columns of table_a that go into the result table for the join
+// // Columns of table_b remain unchanged in the result table for the one-to-many join case 
+// pub fn calc_final_join_one_to_many_advice<E, PCS>(
+//     prover_tracker: &mut ProverTrackerRef<E, PCS>,
+//     table_a: &Table<E, PCS>, // primary key table, no duplicates
+//     table_b: &Table<E, PCS>, // foreign key table, has duplicates
+//     a_join_col_index: usize,
+//     b_join_col_index: usize,
+// ) -> Result<Vec<DenseMultilinearExtension<E::ScalarField>>, PolyIOPErrors>
+// where
+//     E: Pairing,
+//     PCS: PolynomialCommitmentScheme<E>,
+// {
+//     let mut transformed_a = Vec::<DenseMultilinearExtension<E::ScalarField>>::with_capacity(table_a.col_vals.len());
+
+//     // calculate the index transform that should be applied to table_a to get rows of the result table 
+//     let index_transform = calc_final_join_one_to_many_index_transform(table_a, table_b, a_join_col_index, b_join_col_index)?;
+
+//     // apply in index_transform to each column of table_a
+//     for col in table_a.col_vals.iter() {
+
+
+//     Ok(())
+// }
+
+// Calculate the index transform that should be applied to table_a to get rows of the result table 
+// returns a list of indices I such that 
+// I[x] = y means that the xth element of the transformed row should come from the yth row of table_a
+pub fn calc_final_join_one_to_many_index_transform<E, PCS>(
+    table_a: &Table<E, PCS>, // primary key table, no duplicates
+    table_b: &Table<E, PCS>, // foreign key table, has duplicates
+    a_join_col_index: usize,
+    b_join_col_index: usize,
+) -> Result<Vec<usize>, PolyIOPErrors>
+where
+    E: Pairing,
+    PCS: PolynomialCommitmentScheme<E>,
+{
+    // create a hashmap from the values of the table_a join column to their index in the table_a
+    let mut a_join_col_hashmap: HashMap<E::ScalarField, usize> = HashMap::new();
+    let a_join_col_evals = table_a.col_vals[a_join_col_index].evaluations();
+    let a_sel_evals = table_a.selector.evaluations();
+    for i in 0..table_a.col_vals.len() {
+        if a_sel_evals[i] == E::ScalarField::one() { // only include activated rows
+            let val = a_join_col_evals[i];
+            a_join_col_hashmap.insert(val, i);
+        }
+    }
+
+    // iterate through the table_b col vals and get the index of the matching row in table_a
+    let b_join_col_evals = table_b.col_vals[b_join_col_index].evaluations();
+    let b_sel_evals = table_b.selector.evaluations();
+    let table_b_col_len = b_join_col_evals.len();
+    let mut index_transform = Vec::<usize>::with_capacity(table_b_col_len);
+    for i in 0..table_b_col_len {
+        if b_sel_evals[i] == E::ScalarField::one() { // only include activated rows
+            let b_val = b_join_col_evals[i];
+            let a_index = a_join_col_hashmap.get(&b_val);
+            if a_index.is_none() {
+                return Err(PolyIOPErrors::InvalidParameters(format!("FinalJoinOneToManyIOP Error: b_join_col_index {} does not match any row in table_a", b_join_col_index)));
+            }
+            index_transform.push(*a_index.unwrap());
+        } else {
+            index_transform.push(0); // row not activated, so put in a dummy index
+        }
+    }
+
+    Ok(index_transform)
+}
+
+pub fn calc_index_transformed_bag<E, PCS>(
+    index_transform: &Vec<usize>,
+    old_poly: &TrackedPoly<E, PCS>,
+    new_activator: &TrackedPoly<E, PCS>,
+) -> Result<DenseMultilinearExtension<E::ScalarField>, PolyIOPErrors>
+where
+    E: Pairing,
+    PCS: PolynomialCommitmentScheme<E>,
+{
+    let old_poly_evals = old_poly.evaluations();
+    let new_activator_evals = new_activator.evaluations();
+    let new_len = index_transform.len();
+
+    #[cfg(debug_assertions)] 
+    {
+        assert_eq!(new_activator_evals.len(), new_len);
+    }
+    
+    let mut transformed_poly_evals = Vec::<E::ScalarField>::with_capacity(new_len);
+    for i in 0..new_len {
+        if new_activator_evals[i] == E::ScalarField::one() {
+            transformed_poly_evals.push(old_poly_evals[index_transform[i]]);
+        } else {
+            transformed_poly_evals.push(E::ScalarField::zero());
+        }
+    }
+
+    let new_mle = DenseMultilinearExtension::from_evaluations_vec(new_activator.num_vars(), transformed_poly_evals);
+    
+    Ok(new_mle)
+}
