476f36eefcdc6ab01d4ea0b9f0b9ca50622d4374
ssolit44 <ssolit44@gmail.com>
2024-08-14 12:52:46 -0400

    group by added tests to check if support/grouping is wrong

--- zksql/src/zksql_poly_iop/group_by/test.rs
+++ zksql/src/zksql_poly_iop/group_by/test.rs
@@ -18,9 +18,123 @@ mod test {
         
     };
 
-    // fn test_group_by_bad_support() -> Result<(), PolyIOPErrors> {
+    fn test_group_by_bag_grouping() -> Result<(), PolyIOPErrors> {
+        // testing params
+        let range_nv = 10;
+        let mut rng = test_rng();
 
-    // }
+        // PCS params
+        let srs = MultilinearKzgPCS::<Bls12_381>::gen_srs_for_testing(&mut rng, range_nv)?;
+        let (pcs_prover_param, pcs_verifier_param) = MultilinearKzgPCS::<Bls12_381>::trim(&srs, None, Some(10))?;
+
+        // create trackers
+        let mut prover_tracker: ProverTrackerRef<Bls12_381, MultilinearKzgPCS<Bls12_381>> = ProverTrackerRef::new_from_pcs_params(pcs_prover_param);
+        let mut verifier_tracker: VerifierTrackerRef<Bls12_381, MultilinearKzgPCS<Bls12_381>> = VerifierTrackerRef::new_from_pcs_params(pcs_verifier_param);
+
+        // create the range poly and its multiplicity vector
+        let range_poly_evals = (0..2_usize.pow(range_nv as u32)).map(|x| Fr::from(x as u64)).collect(); // numbers are between 0 and 2^10 by construction
+        let range_mle = DenseMultilinearExtension::from_evaluations_vec(range_nv, range_poly_evals);
+        let range_sel_mle = DenseMultilinearExtension::from_evaluations_vec(range_nv, vec![Fr::one(); 2_usize.pow(range_nv as u32)]);
+
+        // set up testing basics 
+        let pre_nv = 3; 
+        let pre_group_nums = vec![1, 1, 1, 2, 2, 3, 0, 0];
+        let pre_sel_nums = vec![1, 1, 1, 1, 1, 1, 0, 0];
+        let pre_agg_nums = vec![11, 12, 13, 14, 15, 16, 0, 0];
+        let support_col_nums = vec![0, 0, 0, 0, 0, 1, 2, 3];
+        let support_sel_nums = vec![0, 0, 0, 0, 0, 1, 1, 1];
+        let support_mult_nums = vec![0, 0, 0, 0, 0, 3, 2, 1];
+        let agg_nums = support_mult_nums.clone(); // for count its the same as support_mult_nums, this is actually a dummy var
+
+        let pre_group_vals = pre_group_nums.iter().map(|x| Fr::from(*x as u64)).collect::<Vec<Fr>>();
+        let pre_group_sel_vals = pre_sel_nums.iter().map(|x| Fr::from(*x as u64)).collect::<Vec<Fr>>();
+        let pre_agg_vals = pre_agg_nums.iter().map(|x| Fr::from(*x as u64)).collect::<Vec<Fr>>();
+        let support_col_vals = support_col_nums.iter().map(|x| Fr::from(*x as u64)).collect::<Vec<Fr>>();
+        let support_sel_vals = support_sel_nums.iter().map(|x| Fr::from(*x as u64)).collect::<Vec<Fr>>();
+        let support_mult_vals = support_mult_nums.iter().map(|x| Fr::from(*x as u64)).collect::<Vec<Fr>>();
+        let agg_vals = agg_nums.iter().map(|x| Fr::from(*x as u64)).collect::<Vec<Fr>>();
+
+        let pre_group_mle = DenseMultilinearExtension::from_evaluations_vec(pre_nv, pre_group_vals.clone());
+        let pre_group_sel_mle = DenseMultilinearExtension::from_evaluations_vec(pre_nv, pre_group_sel_vals.clone());
+        let pre_agg_mle = DenseMultilinearExtension::from_evaluations_vec(pre_nv, pre_agg_vals.clone());
+        let support_col_mle = DenseMultilinearExtension::from_evaluations_vec(pre_nv, support_col_vals.clone());
+        let support_sel_mle = DenseMultilinearExtension::from_evaluations_vec(pre_nv, support_sel_vals.clone());
+        let support_mult_mle = DenseMultilinearExtension::from_evaluations_vec(pre_nv, support_mult_vals.clone());
+        let agg_mle = DenseMultilinearExtension::from_evaluations_vec(pre_nv, agg_vals.clone());
+
+        let table_vals = vec![pre_group_mle.clone(), pre_agg_mle.clone()];
+        let table_sel = pre_group_sel_mle.clone();
+        let grouping_cols = vec![0];
+        let agg_mle_instructions = vec![(0, AggregationType::Count, agg_mle.clone())];
+
+        // Test bad grouping 1: bad support_col_nums
+        print!("GroupByIOP bad grouping 1 test: ");
+        let support_col_nums = vec![0, 0, 0, 0, 0, 4, 2, 3]; // changed a 1 to a 4 here 
+        let bad_support_col_vals = support_col_nums.iter().map(|x| Fr::from(*x as u64)).collect::<Vec<Fr>>();
+        let bad_support_col_mle = DenseMultilinearExtension::from_evaluations_vec(pre_nv, bad_support_col_vals.clone());
+        let bad_res_1 = test_group_by_helper(
+            &mut prover_tracker, 
+            &mut verifier_tracker, 
+            &table_vals, 
+            &table_sel, 
+            &grouping_cols, 
+            &vec![bad_support_col_mle.clone()], 
+            &support_sel_mle, 
+            &support_mult_mle, 
+            &agg_mle_instructions, 
+            &range_mle, 
+            &range_sel_mle, 
+            range_nv,
+        );
+        assert!(bad_res_1.is_err());
+        println!("passed");
+
+        // Test bad grouping 2: bad support_sel_nums
+        print!("GroupByIOP bad grouping 2 test: ");
+        let support_sel_nums = vec![0, 0, 0, 0, 0, 1, 1, 0]; // changes the last 1 to a 0
+        let bad_support_sel_vals = support_sel_nums.iter().map(|x| Fr::from(*x as u64)).collect::<Vec<Fr>>();
+        let bad_support_sel_mle = DenseMultilinearExtension::from_evaluations_vec(pre_nv, bad_support_sel_vals.clone());
+        let bad_res_2 = test_group_by_helper(
+            &mut prover_tracker, 
+            &mut verifier_tracker, 
+            &table_vals, 
+            &table_sel, 
+            &grouping_cols, 
+            &vec![bad_support_sel_mle.clone()], 
+            &support_sel_mle, 
+            &support_mult_mle, 
+            &agg_mle_instructions, 
+            &range_mle, 
+            &range_sel_mle, 
+            range_nv,
+        );
+        assert!(bad_res_2.is_err());
+        println!("passed");
+
+        // Test bad grouping 3: bad support_mult_nums
+        print!("GroupByIOP bad grouping 3 test: ");
+        let support_mult_nums = vec![0, 0, 0, 0, 0, 3, 2, 2]; // changes the last 1 to a 2
+        let bad_support_mult_vals = support_mult_nums.iter().map(|x| Fr::from(*x as u64)).collect::<Vec<Fr>>();
+        let bad_support_mult_mle = DenseMultilinearExtension::from_evaluations_vec(pre_nv, bad_support_mult_vals.clone());
+        let bad_res_3 = test_group_by_helper(
+            &mut prover_tracker, 
+            &mut verifier_tracker, 
+            &table_vals, 
+            &table_sel, 
+            &grouping_cols, 
+            &vec![bad_support_mult_mle.clone()], 
+            &support_sel_mle, 
+            &support_mult_mle, 
+            &agg_mle_instructions, 
+            &range_mle, 
+            &range_sel_mle, 
+            range_nv,
+        );
+        assert!(bad_res_3.is_err());
+        println!("passed");
+
+        Ok(())
+    }
 
     fn test_group_by_count() -> Result<(), PolyIOPErrors> {
         // testing params
@@ -282,7 +396,11 @@ mod test {
         Ok(())
     }
 
-    
+    #[test]
+    fn group_by_bad_grouping_test() {
+        let res = test_group_by_bag_grouping();
+        res.unwrap();
+    }
 
     #[test]
     fn group_by_count_test() {
